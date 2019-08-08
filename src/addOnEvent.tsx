de997eb4296116c5b159c7fd89e701be2ddb6609
svpcadmin <PhilisPaxil@gmail.com>
2019-08-08 19:16:18 +0900

    [fix]bug to perfoem multi

--- src/addOnEvent.tsx
+++ src/addOnEvent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, ReactChild, ReactChildren } from "react";
+import React, { useEffect, ReactChild, ReactChildren, useState } from "react";
 
 function ensure<T>(
 	arg: T | undefined | null,
@@ -20,20 +20,26 @@ export default function AddOnEvent(props: {
 	onShiftEnter?: Function;
 	onControlEnter?: Function;
 }) {
+	const [flag, handleFlag] = useState(false);
 	useEffect(() => {
 		const targetDocument = ensure(document.querySelector(".AddonEventWrapper"));
 
-		
 		if (props.onEnter || props.onShiftEnter || props.onControlEnter) {
-			targetDocument.addEventListener("keydown", (event: Keyboard) => {
-				if (!event.isComposing) {
-					if (event.code === "Enter") {
-						if (event.shiftKey && props.onShiftEnter) {
-							return props.onShiftEnter();
-						} else if (event.metaKey && props.onControlEnter) {
-							return props.onControlEnter();
-						} else if(props.onEnter){
-							return props.onEnter();
+			targetDocument.addEventListener("keydown", async (event: Keyboard) => {
+				if (!flag) {
+					await handleFlag(true);
+					if (!event.isComposing) {
+						if (event.code === "Enter") {
+							if (event.shiftKey && props.onShiftEnter) {
+								await props.onShiftEnter();
+								return handleFlag(false);
+							} else if (event.metaKey && props.onControlEnter) {
+								await props.onControlEnter();
+								return handleFlag(false);
+							} else if (props.onEnter) {
+								await props.onEnter();
+								return handleFlag(false);
+							}
 						}
 					}
 				}
